08b57dc0ecbcf5d4b8c13b79c93cfb9d1ef0b667
zootsuitproductions <dannysantanasf@gmail.com>
2018-02-20 21:40:22 -0800

    Update sketch.js

--- filter/sketch.js
+++ filter/sketch.js
@@ -2,6 +2,7 @@ var img;
 let gFile;
 
 //code from Max Wallace https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+//it loads an image selected by the computer into browser--I was unable to implement this feature into my final project, though
 
 function previewFile(){
     var preview = document.querySelector('img'); //selects the query named img
